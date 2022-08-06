c31db5c175ee097427b19b225daa022964c6f45d
lina-22 <haquefahinur@gmail.com>
2022-08-06 11:30:02 +0200

    pattern

---
+++ Array/src/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int [] number = new int[5];
+        number[0]=10;
+        number[1]=10;
+        number[2]=10;
+        number[3]=10;
+        number[4]=10;
+
+        int len = number.length;
+        System.out.println(len);
+    }
+}
---
+++ Array/src/Array2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Array2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+      double[] number = new double[5];
+      double sum =0;
+        System.out.print("please enter 5 numbers:");
+
+        for (int i = 0; i <5 ; i++) {
+           number[i] = input.nextDouble();
+        }
+
+        sum = number[0] + number[2] + number[3] + number[4];
+        System.out.println("The sum is "+sum);
+    }
+}
